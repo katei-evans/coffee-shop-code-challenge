07b57f447d349ff34dbeaec2de41665344f19989
Katei <kateievans28@gmail.com>
2025-05-19 14:12:16 +0300

    customer,order,coffee-done

---
+++ pipfile/coffee.py
@@ -0,0 +1,29 @@
+class Coffee:
+    all_coffees = []
+
+    def __init__(self, name):
+        if isinstance(name, str) and len(name) >= 3:
+            self._name = name
+            Coffee.all_coffees.append(self)
+        else:
+            raise ValueError("Coffee name must be a string of 3 or more characters")
+
+    @property
+    def name(self):
+        return self._name
+
+    def orders(self):
+        from order import Order
+        return [order for order in Order.all_orders if order.coffee == self]
+
+    def customers(self):
+        return list({order.customer for order in self.orders()})
+
+    def num_orders(self):
+        return len(self.orders())
+
+    def average_price(self):
+        prices = [order.price for order in self.orders()]
+        if not prices:
+            return 0
+        return sum(prices) / len(prices)
---
+++ pipfile/customer.py
@@ -0,0 +1,39 @@
+class Customer:
+    all_customers = []
+
+    def __init__(self, name):
+        self.name = name
+        Customer.all_customers.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 15:
+            self._name = value
+        else:
+            raise ValueError("Customer name must be a string between 1 and 15 characters")
+
+    def orders(self):
+        from order import Order
+        return [order for order in Order.all_orders if order.customer == self]
+
+    def coffees(self):
+        return list({order.coffee for order in self.orders()})
+
+    def create_order(self, coffee, price):
+        from order import Order
+        return Order(self, coffee, price)
+
+    @classmethod
+    def most_aficionado(cls, coffee):
+        customer_spending = {}
+        from order import Order
+        for order in Order.all_orders:
+            if order.coffee == coffee:
+                customer_spending[order.customer] = customer_spending.get(order.customer, 0) + order.price
+        if not customer_spending:
+            return None
+        return max(customer_spending, key=customer_spending.get)
